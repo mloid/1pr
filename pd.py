21d935f501649b5c9e5262826f3af28ad2e31b34
mloid <42334799+mloid@users.noreply.github.com>
2021-08-06 03:18:48 +0300

    Create pd.py

---
+++ pd.py
@@ -0,0 +1,24 @@
+import csv
+
+
+#СОЗДАНИЕ НОВОЙ ТАБЛИЦЫ
+
+#записываем данные в таблицу data
+data = [['hostname', 'vendor', 'model', 'location'],
+        ['sw1', 'Cisco', '3750', 'London, Best str'],
+        ['sw2', 'Cisco', '3850', 'Liverpool, Better str'],
+        ['sw3', 'Cisco', '3650', 'Liverpool, Better str'],
+        ['sw4', 'Cisco', '3650', 'London, Best str']]
+#пишем имя дока
+tablename = 'tablename'
+#сохраняем
+with open(tablename+'.csv', 'w') as f:
+    writer = csv.writer(f)
+    for row in data:
+        writer.writerow(row)
+       
+#    _~_
+#   (o o)
+#  /  V  \
+# /(  _  )\
+#   ^^ ^^
